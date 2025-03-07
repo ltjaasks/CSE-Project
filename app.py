389c486af72e2302402749e6cd7acc464f3cfb8e
ltjaasks <lauri.t.jaaskelainen@gmail.com>
2025-03-07 15:39:27 +0200

    Refactored api-calls in app.py and made some tests.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template, request
+from flask import Flask, render_template, request, make_response
 import requests
 from dotenv import load_dotenv
 import os
@@ -21,7 +20,9 @@ def hello():
             raise ValueError("Error: API key not found. Set WA_API_KEY in your .env file.")
         return render_template('index.html')
     except Exception as error:
-        return render_template('index.html', error_message=error)
+        response = make_response(render_template('index.html', error_message=error), 404)
+        response.mimetype = 'text/html'
+        return response
 
 
 @app.route('/', methods=['POST'])
@@ -29,24 +30,23 @@ def showTemperatures():
     try:
         location = request.form['location'].capitalize().strip(" ")
         print(location)
-        response_owm = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={location}&APPID={api_key_owm}&units=metric')
-        data_owm = response_owm.json()
+        owm = get_temperature_from_api(f'https://api.openweathermap.org/data/2.5/weather?q={location}&APPID={api_key_owm}&units=metric')
 
-        print(data_owm)
+        wa = get_temperature_from_api(f'http://api.weatherapi.com/v1/current.json?key={api_key_wa}&q={location}&aqi=no')
 
-        if data_owm['cod'] != 200:
-            raise ValueError("Enter a valid city name")
-
-        response_wa = requests.get(f'http://api.weatherapi.com/v1/current.json?key={api_key_wa}&q={location}&aqi=no')
-        data_wa = response_wa.json()
-
-        if data_owm['cod'] != 200:
-            raise ValueError("Enter a valid city name")
-
-        owm = data_owm["main"]["temp"]
-        wa = data_wa["current"]["temp_c"]
         difference = round(abs(owm - wa), 2)
         avg = round((owm + wa) / 2, 2)
         return render_template('index.html', owm=owm, wa=wa, difference=difference, avg=avg, location=location)
     except Exception as error:
         return render_template('index.html', error_message=error)
+    
+
+def get_temperature_from_api(api_url):
+    response = requests.get(api_url)
+    data = response.json()
+    if response.status_code != 200:
+        raise ValueError("Enter a valid city name")
+    if "main" in data and "temp" in data["main"]:
+        return data["main"]["temp"]
+    elif "current" in data and "temp_c" in data["current"]:
+        return data["current"]["temp_c"]
--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,8 @@
+import pytest
+from app import app, get_temperature_from_api
+import requests
+import requests_mock
+from unittest.mock import patch
 # What to test:
 # 1. Error cases
 # 2. API calls
@@ -5,3 +10,23 @@
 # 4. Input
 # 5. Output
 # API keys
+
+def test_landing_page_returns_200_and_placeholder_found_from_response():
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert b"Enter a location" in response.data
+
+@patch('requests.get')
+def test_invalid_city_name_raises_value_error(mock_get):
+    mock_response = mock_get.return_value
+    mock_response.status_code = 404
+    mock_response.json_return_value = {
+        "cod":"404",
+        "message":"city not found"
+        }
+    
+    url = 'http://example.com/api'
+
+    with pytest.raises(ValueError):
+        get_temperature_from_api(url)
